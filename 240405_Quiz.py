6aca51a84770d96743b72065fb78caf1509fbc9d
Jeonsangwan <wjstkddhks1@gmail.com>
2024-04-05 15:24:39 +0900

    240405_Quiz
    if조건문

---
+++ 240405_Quiz.py
@@ -0,0 +1,11 @@
+grade = input("학점을 입력하세요 : ")
+num_grade = int(grade)
+
+if num_grade >= 71 :
+    print("A")
+elif num_grade >= 41 :
+    print("B")
+elif num_grade >= 11 :
+    print("C")
+else :
+    print("D")
